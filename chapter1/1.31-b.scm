cb2b156d8f624a64dd7fd0eee58825cc5a98d7b4
cheng33c <asglitc12@gmail.com>
2017-07-15 15:52:33 +0800

    1.31b

---
+++ chapter1/1.31-b.scm
@@ -0,0 +1,34 @@
+#lang planet neil/sicp
+
+(define (product term a next b)
+  (define (rec a result)
+    (if (> a b)
+        result
+        (* (term a) (rec (next a) result))))
+  (rec a 1))
+
+
+(define (factorial n)
+  (product (lambda (x) x)
+           1
+           (lambda (i) (+ i 1))
+           n))
+
+(define (number-term i)
+  (cond ((= i 1) 2)
+        ((even? i) (+ i 2))
+        (else (+ i 1))))
+
+(define (denom-term i)
+  (if (odd? i)
+      (+ i 2)
+      (+ i 1)))
+
+(define (pi n)
+  (* 4
+     (exact->inexact
+      (/ (product number-term 1 (lambda (i) (+ i 1)) n)
+         (product denom-term 1 (lambda (i) (+ i 1)) n)))))
+
+(pi 100)
+(pi 1000)
--- chapter1/1.31.scm
+++ chapter1/1.31.scm
@@ -3,11 +3,11 @@
 (define (square x) (* x x))
 
 (define (product term a next b)
-  (define (mult a result)
+  (define (iter a result)
     (if (> a b)
         result
-        (mult (next a) (* result (term a)))))
-  (mult a 1))
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
 
 (define (factorial b)
   (define (inc2 x) (+ x 2))
@@ -27,14 +27,3 @@
      4))
 
 (exact->inexact (factorial 200))
-
-
-;;; 另一种解 pi / 2 = (* 2 2 4 4 6 6 ...) / (* 3 3 5 5 7 7 ...)
-;;; 不能使用。 查错中.
-
-(define (factorial1 n)
-  (define (next x) (+ x 2))
-
-  (* (/ (product square 2 next n) (product square 3 next n)) 2))
-
-(newline)

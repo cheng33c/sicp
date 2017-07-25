d7e7f2af8dfda550385b97cc07bceb9b36595eed
cheng33c <asglitc12@gmail.com>
2017-07-25 17:08:10 +0800

    2.3

--- chapter2/2.3.scm
+++ chapter2/2.3.scm
@@ -1,26 +1,66 @@
 #lang planet neil/sicp
 
-(define (perimeter-rectangle r)
-  (let ((length (length-of-rectangle r))
-        (width (width-of-rectangle r)))
-    (* 2 (+ length width))))
+(define (square x) (* x x))
 
-(define (area-rectangle r)
-  (* (length-of-rectangle r)
-     (width-of-rectangle r)))
+(define (make-point x y)
+  (cons x y))
 
-(define (make-rectangle length-1 length-2 width-1 width-2)
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (make-segment x1 y1 x2 y2)
+  (cons (make-point x1 y1)
+        (make-point x2 y2)))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+;;; 表示方式1： 用点来表示矩形
+(define (distance-point p1 p2)
+  (sqrt (+
+         (square (- (x-point p1)
+                    (x-point p2)))
+         (square (- (y-point p1)
+                    (y-point p2))))))
+
+(define (area-rectangle p1 p2 p3)
+  (* (distance-point p1 p2)
+     (distance-point p1 p3)))
+
+(define (perimeter-rectangle p1 p2 p3)
+  (* (+ (distance-point p1 p2)
+        (distance-point p1 p3)) 2))
+
+(area-rectangle (make-point 1 1) (make-point 1 3) (make-point 3 1))
+(perimeter-rectangle (make-point 1 1) (make-point 1 3) (make-point 3 1))
+
+;; 表示方式2： 构造矩形用两个线段
+(define (make-rectangle length-1 width-1 length-2 width-2)
   (cons (cons length-1 length-2)
         (cons width-1 width-2)))
 
-(define (length-1-rectangle r)
-  (car (car r)))
+(define (area-rectangle-seg seg-length seg-width)
+  (* (distance-point (start-segment seg-width)
+                     (end-segment seg-width))
+     (distance-point (start-segment seg-length)
+                     (end-segment seg-length))))
 
-(define (length-2-rectangle r)
-  (cdr (car r)))
+(define (perimeter-rectangle-seg seg-length seg-width)
+  (* (+ (distance-point (start-segment seg-width)
+                     (end-segment seg-width))
+        (distance-point (start-segment seg-length)
+                     (end-segment seg-length)))
+     2))
 
-(define (length-1-rectangle r)
-  (car (cdr r)))
+(make-rectangle (make-segment 1 2 1 5)
+                (make-segment 1 2 8 2)
+                (make-segment 8 2 8 5)
+                (make-segment 8 5 1 5))
 
-(define (width-2-rectangle r)
-  (cdr (cdr r)))
+(area-rectangle-seg (make-segment 1 2 1 5) (make-segment 1 2 8 2))

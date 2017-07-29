6267566babbb2114078094b5f3fefdfb3b6d8250
cheng33c <asglitc12@gmail.com>
2017-07-29 12:44:35 +0800

    scale-list

---
+++ chapter2/scale-list.scm
@@ -0,0 +1,9 @@
+#lang planet neil/sicp
+
+(define (scale-list items factor)
+  (if (null? items)
+      nil
+      (cons (* (car items) factor)
+            (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)

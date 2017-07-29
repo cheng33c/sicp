27ffb7e0f7ba1a8aac724f22d9d3b491581b4585
cheng33c <asglitc12@gmail.com>
2017-07-29 12:53:22 +0800

    2.21

---
+++ chapter2/2.21.scm
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list (cdr items)))))
+
+(define (square-list-map items)
+  (map (lambda (x) (* x x)) items))
+
+(square-list (list 1 2 3 4 5))
+
+(square-list-map (list 1 2 3 4 5))

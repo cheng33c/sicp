215b92f24a0c92e15bfd3a533301bad7259ac351
cheng33c <asglitc12@gmail.com>
2017-07-29 12:47:51 +0800

    map.scm

---
+++ chapter2/map.scm
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+
+(map (lambda (x) (* x x))
+     (list 1 2 3 4))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+
+(scale-list (list 1 2 3 4 5) 10)

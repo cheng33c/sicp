5f5027ae49f12e916ad0105e7031bde40eddfb50
cheng33c <asglitc12@gmail.com>
2017-08-02 09:40:36 +0800

    enumerate-tree.scm

--- chapter2/enumerate.scm
+++
@@ -1,9 +0,0 @@
-#lang planet neil/sicp
-
-(define (enumerate-tree tree)
-  (cond ((null? tree) nil)
-        ((not (pair? tree)) (list tree))
-        (else (append (enumerate-tree (car tree))
-                      (enumerate-tree (cdr tree))))))
-
-(enumerate-tree (list 1 (list 2 (list 3 4)) 5))

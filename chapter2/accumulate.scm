5e6c7653c8f0fcde4b58c0d36c88e3cd9285a2a9
cheng33c <asglitc12@gmail.com>
2017-08-02 09:36:08 +0800

    accumulate.scm

---
+++ chapter2/accumulate.scm
@@ -0,0 +1,13 @@
+#lang planet neil/sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(accumulate + 0 (list 1 2 3 4 5))
+
+(accumulate * 1 (list 1 2 3 4 5))
+
+(accumulate cons nil (list 1 2 3 4 5))
---
+++ chapter2/filter.scm
@@ -0,0 +1,10 @@
+#lang planet neil/sicp
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(filter odd? (list 1 2 3 4 5))

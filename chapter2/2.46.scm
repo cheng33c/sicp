3a17c0e4129f8ec4264294673c6062e6051e2d0b
cheng33c <asglitc12@gmail.com>
2017-08-09 15:19:31 +0800

    2.46.scm

---
+++ chapter2/2.46.scm
@@ -0,0 +1,32 @@
+#lang planet neil/sicp
+
+(define (make-vect x y) (list x y))
+
+(define (xcor-vect vec) (car vec))
+
+(define (ycor-vect vec) (cadr vec))
+
+(define (add-vect vec1 vec2)
+  (cons (+ (xcor-vect vec1) (xcor-vect vec2))
+        (+ (ycor-vect vec1) (ycor-vect vec2))))
+
+(define (sub-vect vec1 vec2)
+  (cons (- (xcor-vect vec1) (xcor-vect vec2))
+        (- (ycor-vect vec1) (ycor-vect vec2))))
+
+(define (scale-vect s vec)
+  (cons (* (xcor-vect vec1) s)
+        (* (ycor-vect vec1) s)))
+
+
+
+;; test
+(define vec1 (make-vect 2 9))
+(define vec2 (make-vect 1 3))
+(define s 7)
+
+(xcor-vect vec1)
+(ycor-vect vec2)
+(add-vect vec1 vec2)
+(sub-vect vec1 vec2)
+(scale-vect s vec1)

c89246bc528ef111e67d9680293c474d7afd70c1
cheng33c <asglitc12@gmail.com>
2017-08-10 16:22:32 +0800

    shrink-to-upper-right.scm

---
+++ chapter2/shrink-to-upper-right.scm
@@ -0,0 +1,22 @@
+#lang planet neil/sicp
+
+
+;; require
+(define (sub-vect vec1 vec2)
+  (map - vec1 vec2))
+
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (shrink-to-upper-right painter)
+  (transform-painter painter
+                     (make-vect 0.5 0.5)
+                     (make-vect 1.0 0.5)
+                     (make-vect 0.5 1.0)))

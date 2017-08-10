06b6c0b16793eaf665ce1905ba642c6095482255
cheng33c <asglitc12@gmail.com>
2017-08-10 18:13:09 +0800

    add : split.scm

---
+++ chapter2/split.scm
@@ -0,0 +1,9 @@
+#lang racket
+
+
+(define (split proc1 proc2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (split painter (- n 1))))
+          (proc1 painter (proc2 smaller smaller))))))
--- chapter2/square-limit2.scm
+++ chapter2/square-limit2.scm
@@ -1,26 +1,5 @@
 #lang planet neil/sicp
 
-(define (split proc1 proc2)
-  (lambda (painter n)
-    (if (= n 0)
-        painter
-        (let ((smaller (split painter (- n 1))))
-          (proc1 painter (proc2 smaller smaller))))))
-
-(define right-split (split beside below))
-(define up-split (split below beside))
-
-(define (corner-split painter n)
-  (if (= n 0)
-      painter
-      (let ((up (up-split painter (- n 1)))
-            (right (right-split painter (- n 1))))
-        (let ((top-left (beside up up))
-              (bottom-right (below right right))
-              (corner (corner-split painter (- n 1))))
-          (beside (below painter top-left)
-                  (below bottom-right corner))))))
-
 
 (define (square-of-four tl tr bl br)
   (lambda (painter)

0a80d75c2fa61fdb0e280bb1102a0693f115ff66
cheng33c <asglitc12@gmail.com>
2017-08-10 16:24:48 +0800

    insert : beside.scm

--- chapter2/beside.scm
+++ chapter2/beside.scm
@@ -5,7 +5,6 @@
 (define (sub-vect vec1 vec2)
   (map - vec1 vec2))
 
-
 (define (transform-painter painter origin corner1 corner2)
   (lambda (frame)
     (let ((m (frame-coord-map frame)))
@@ -14,12 +13,6 @@
          (make-frame new-origin
                      (sub-vect (m corner1) new-origin)
                      (sub-vect (m corner2) new-origin)))))))
-
-(define (squash-inwards painter)
-  (transform-painter painter
-                     (make-vect 0.0 0.0)
-                     (make-vect 0.65 0.35)
-                     (make-vect 0.35 0.65)))
 
 (define (beside painter1 painter2)
   (let ((split-point (make-vect 0.5 0.0)))

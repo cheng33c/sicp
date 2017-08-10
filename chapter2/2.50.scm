d73a3f67fcf0256ac48a16126ad9106da1eaa8f1
cheng33c <asglitc12@gmail.com>
2017-08-10 16:44:34 +0800

    add : 2.50.scm 2.50O.scm

---
+++ chapter2/2.50.scm
@@ -0,0 +1,33 @@
+#lang planet neil/sicp
+
+;; require
+(define (sub-vect vec1 vec2)
+  (map - vec1 vec2))
+
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))

ec756e5d7c51265ea2c6cf5e303db7f584a6ce90
cheng33c <asglitc12@gmail.com>
2017-08-10 18:56:26 +0800

    square-limit2.scm

--- chapter2/square-limit2.scm
+++ chapter2/square-limit2.scm
@@ -1,5 +1,15 @@
 #lang planet neil/sicp
 
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
 
 (define (square-of-four tl tr bl br)
   (lambda (painter)

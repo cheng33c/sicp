34928274485fc655e24206e9756611a8691ce970
cheng33c <asglitc12@gmail.com>
2017-08-10 14:26:49 +0800

    2.49

--- chapter2/2.49.scm
+++ chapter2/2.49.scm
@@ -1,112 +1,155 @@
-#lang planet neil/sicp
+#lang racket/gui
+
+(require graphics/graphics)
+(open-graphics)
+(define vp (open-viewport "2.49 Solution" 500 500))
+
+(define draw (draw-viewport vp))
+(define (clear) ((clear-viewport vp)))
+(define line (draw-line vp))
+(define (drawline begine end)
+  (line
+   (make-posn (car begine) (cadr begine)) 
+   (make-posn (car end) (cadr end)))
+  )
+
+;; require
+(define (make-vect x y) (list x y))
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (make-segment vec1 vec2)
+  (list vec1 vec2))
+
+(define (xcor-vect vec) (car vec))
+
+(define (ycor-vect vec) (cadr vec))
+
+(define (origin-frame vec) (car vec))
+
+(define (make-frame1 origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (edge1-frame frame) (cadr frame))
+
+(define (edge2-frame frame) (caddr frame))
+
+(define (add-vect vec1 vec2)
+  (map + vec1 vec2))
+
+(define (sub-vect vec1 vec2)
+  (map - vec1 vec2))
+
+(define (scale-vect s vec)
+  (map (lambda (v) (* s v)) vec))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cadr segment))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (drawline
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
+(define outline-painter
+  (let ((a (make-vect 0 0))
+        (b (make-vect 0 1))
+        (c (make-vect 1 0))
+        (d (make-vect 1 1)))
+    (let ((segment-list
+           (list (make-segment a b)
+                 (make-segment a c)
+                 (make-segment b d)
+                 (make-segment c d))))
+      (segments->painter segment-list))))
 
 
-(define top-left (make-vect 0.0 1.0))
+(define fork-painter
+  (let ((a (make-vect 0 0))
+        (b (make-vect 0 1))
+        (c (make-vect 1 0))
+        (d (make-vect 1 1)))
+    (let ((segment-list
+           (list (make-segment a d)
+                 (make-segment b c))))
+      (segments->painter segment-list))))
 
-(define top-right (make-vect 1.0 1.0))
+(define diamond-painter
+  (let ((a (make-vect 0 0.5))
+        (b (make-vect 0.5 0))
+        (c (make-vect 1 0.5))
+        (d (make-vect 0.5 1)))
+    (let ((segment-list
+           (list (make-segment a b)
+                 (make-segment a d)
+                 (make-segment b c)
+                 (make-segment c d))))
+      (segments->painter segment-list))))
 
-(define bottom-left (make-vect 0.0 0.0))
+(define wave-painter
+  (let ((a (make-vect 0.4 0))
+        (b (make-vect 0.6 0))
+        (c (make-vect 0.25 0.2))
+        (d (make-vect 0.75 0.2))
+        (e (make-vect 0.45 0.4))
+        (f (make-vect 0.65 0.4))
+        (g (make-vect 0.75 0.4))
+        (h (make-vect 0.35 0.4))
+        (i (make-vect 0.25 0.35))
+        (j (make-vect 0 0.2))
+        (k (make-vect 1 0.7))
+        (l (make-vect 0.3 0.7))
+        (m (make-vect 1 0.8))
+        (x (make-vect 0.35 0.45)) ;; 这个点是忘记了，临时添加的
+        (n (make-vect 0.4 0.5))
+        (o (make-vect 0.65 0.6))
+        (p (make-vect 0.35 1))
+        (q (make-vect 0.5 1))
+        (r (make-vect 0.55 0.75))
+        (s (make-vect 0.6 1))
+        (t (make-vect 0.7 1))
+        (y (make-vect 0 0.3)))
+    (let ((segment-list
+           (list (make-segment a c)
+                 (make-segment b d)
+                 (make-segment c e)
+                 (make-segment d f)
+                 (make-segment e h)
+                 (make-segment f g)
+                 (make-segment h i)
+                 (make-segment i j)
+                 (make-segment g k)
+                 (make-segment y l)
+                 (make-segment l x)
+                 (make-segment x n)
+                 (make-segment n p)
+                 (make-segment q r)
+                 (make-segment r s)
+                 (make-segment t o)
+                 (make-segment o m))))
+      (segments->painter segment-list))))
 
-(define bottom-right (make-vect 1.0 0.0))
-
-(define top (make-segment top-left top-right))
-
-(define left (make-segment top-left bottom-left))
-
-(define right (make-segment top-right bottom-right))
-
-(define bottom (make-segment bottom-left bottom-right))
-
-(segments->painter (list top bottom left right))
-
-
-
-
-(define left-top (make-vect 0.0 1.0))
-
-(define right-bottom (make-vect 1.0 0.0))
-
-(define right-top (make-vect 1.0 1.0))
-
-(define left-bottom (make-vect 0.0 0.0))
-
-(define left-top-to-right-bottom (make-segment left-top
-                                               right-bottom))
-
-(define right-top-to-left-bottom (make-segment right-top
-                                               left-bottom))
-
-(segments->painter (list left-top-to-right-bottom
-                         right-top-to-left-bottom))
-
-
-
-
-(define top-mid-point (make-vect 0.5 1.0))
-
-(define bottom-mid-point (make-vect 0.5 0.0))
-
-(define left-mid-point (make-vect 0.0 0.5))
-
-(define right-mid-point (make-vect 1.0 0.5))
-
-(define top-to-left (make-segment top-mid-point
-                                  left-mid-point))
-
-(define top-to-right (make-segment top-mid-point
-                                   right-mid-point))
-
-(define bottom-to-left (make-segment bottom-mid-point
-                                     left-mid-point))
-
-(define bottom-to-right (make-segment bottom-mid-point
-                                      right-mid-point))
-
-(segments->painter (list top-to-left
-                         top-to-right
-                         bottom-to-left
-                         bottom-to-right))
-
-
-
-
-
-(segments->painter (list
-                         (make-segment (make-vect 0.4 1.0)      ; 头部左上
-                                       (make-vect 0.35 0.85))
-                         (make-segment (make-vect 0.35 0.85)    ; 头部左下
-                                       (make-vect 0.4 0.64))
-                         (make-segment (make-vect 0.4 0.65)     ; 左肩
-                                       (make-vect 0.25 0.65))
-                         (make-segment (make-vect 0.25 0.65)    ; 左手臂上部
-                                       (make-vect 0.15 0.6))
-                         (make-segment (make-vect 0.15 0.6)     ; 左手上部
-                                       (make-vect 0.0 0.85))
-
-                         (make-segment (make-vect 0.0 0.65)     ; 左手下部
-                                       (make-vect 0.15 0.35))
-                         (make-segment (make-vect 0.15 0.35)    ; 左手臂下部
-                                       (make-vect 0.25 0.6))
-
-                         (make-segment (make-vect 0.25 0.6)     ; 左边身体
-                                       (make-vect 0.35 0.5))
-                         (make-segment (make-vect 0.35 0.5)     ; 左腿外侧
-                                       (make-vect 0.25 0.0))
-                         (make-segment (make-vect 0.6 1.0)      ; 头部右上
-                                       (make-vect 0.65 0.85))
-                         (make-segment (make-vect 0.65 0.85)    ; 头部右下
-                                       (make-vect 0.6 0.65))
-                         (make-segment (make-vect 0.6 0.65)     ; 右肩
-                                       (make-vect 0.75 0.65))
-                         (make-segment (make-vect 0.75 0.65)    ; 右手上部
-                                       (make-vect 1.0 0.3))
-
-                         (make-segment (make-vect 1.0 0.15)     ; 右手下部
-                                       (make-vect 0.6 0.5))
-                         (make-segment (make-vect 0.6 0.5)      ; 右腿外侧
-                                       (make-vect 0.75 0.0))
-
-                         (make-segment (make-vect 0.4 0.0)      ; 左腿内侧
-                                       (make-vect 0.5 0.3))
-                         (make-segment (make-vect 0.6 0.0)      ; 右腿内侧
-                                       (make-vect 0.5 0.3))))
+;; test outline-drawer
+;; define a frame where painter draw in
+(define frame-a (make-frame (make-vect 0 200) (make-vect 200 0) (make-vect 0 200)))
+;;(outline-painter frame-a)
+;;(fork-painter frame-a)
+;;(diamond-painter frame-a)
+(wave-painter frame-a)
---
+++ chapter2/2.49O2.scm
@@ -0,0 +1,158 @@
+#lang racket/gui
+
+(require graphics/graphics)
+(open-graphics)
+(define vp (open-viewport "A Picture Language" 500 500))
+
+(define draw (draw-viewport vp))
+(define (clear) ((clear-viewport vp)))
+(define line (draw-line vp))
+(define (drawline begine end)
+    (line
+        (make-posn (car begine) (cadr begine)) 
+        (make-posn (car end) (cadr end)))
+)
+
+(define (make_vect x y)
+    (list x y)
+)
+
+(define (xcor_vect vect) (car vect))
+(define (ycor_vect vect) (cadr vect))
+(define (add_vect vect1 vect2)
+    (map + vect1 vect2)
+)
+
+(define (sub_vect vect1 vect2)
+    (map - vect1 vect2)
+)
+
+(define (scale_vect n vect)
+    (map (lambda (v) (* v n)) vect)
+)
+
+(define (frame_coord_map frame)
+  (lambda (v)
+    (add_vect
+     (origin_frame frame)
+     (add_vect 
+      (scale_vect (xcor_vect v)
+                  (edge1_frame frame))
+      (scale_vect (ycor_vect v)
+                  (edge2_frame frame))))))
+                  
+(define (make_segment vector1 vector2)
+    (list vector1 vector2)
+)
+
+(define (start_segment seg)
+    (car seg)
+)
+(define (end_segment seg)
+    (cadr seg)
+)
+
+(define (make_frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (make_frame1 origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin_frame frame) (car frame))
+(define (edge1_frame frame) (cadr frame))
+(define (edge2_frame frame) (caddr frame))
+
+(define (segments_>painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (drawline
+        ((frame_coord_map frame) 
+         (start_segment segment))
+        ((frame_coord_map frame) 
+         (end_segment segment))))
+     segment-list)))
+
+
+(define unit_frame (make_frame (make_vect 0 100) (make_vect 100 0) (make_vect 0 100)))
+
+;1 outline
+(define outline_drawer
+    (let 
+        (
+            (a (make_vect 0 0))
+            (b (make_vect 0 1))
+            (c (make_vect 1 0))
+            (d (make_vect 1 1))
+            
+        )
+        (let
+            (
+                (
+                    segment_list 
+                    (list 
+                        (make_segment a b)
+                        (make_segment a c)
+                        (make_segment b d)
+                        (make_segment c d)
+                    )
+                )
+            )
+            (segments_>painter segment_list)
+        )
+    )
+)
+; 2x
+(define X_drawer
+    (let 
+        (
+            (a (make_vect 0 0))
+            (b (make_vect 0 1))
+            (c (make_vect 1 0))
+            (d (make_vect 1 1))
+            
+        )
+        (let
+            (
+                (
+                    segment_list 
+                    (list 
+                        (make_segment a d)
+                        (make_segment b c)
+                    )
+                )
+            )
+            (segments_>painter segment_list)
+        )
+    )
+)
+; 3 diamond
+(define D_drawer
+    (let 
+        (
+            (a (make_vect 0.5 0))
+            (b (make_vect 0 0.5))
+            (c (make_vect 0.5 1))
+            (d (make_vect 1 0.5))
+            
+        )
+        (let
+            (
+                (
+                    segment_list 
+                    (list 
+                        (make_segment a b)
+                        (make_segment a d)
+                        (make_segment b c)
+                        (make_segment c d)
+                    )
+                )
+            )
+            (segments_>painter segment_list)
+        )
+    )
+)
+; 4.wave(I dont want to draw - -//// onz)
+
+
+(outline_drawer unit_frame)
+(D_drawer unit_frame)

d7e1bb6293f0bd16f97f87f8011a8055c5493d00
cheng33c <asglitc12@gmail.com>
2017-08-11 12:47:42 +0800

    deriv2.scm

--- chapter2/deriv2.scm
+++ chapter2/deriv2.scm
@@ -3,8 +3,6 @@
 (define (variable? x) (symbol? x))
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
-(define (make-sum a1 a2) (list '+ a1 a2))
-(define (make-product m1 m2) (list '* m1 m2))
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 (define (addend s) (cadr s))
@@ -14,6 +12,24 @@
 (define (multiplier p) (cadr p))
 (define (multiplicand p) (caddr p))
 
+;; insert produce `make-sum`
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+;; add produce `=number?`
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+;; insert product `make-product`
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)

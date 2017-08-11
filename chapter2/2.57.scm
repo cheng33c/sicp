fc265b1d5509cdff5e97e890ed0c672aac525647
cheng33c <asglitc12@gmail.com>
2017-08-11 16:25:27 +0800

    insert : make 2.57.scm right

--- chapter2/2.57.scm
+++ chapter2/2.57.scm
@@ -4,25 +4,28 @@
 (define (variable? x) (symbol? x))
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
 
 (define (addend s) (cadr s))
-(define (augend s) (cddr s))
 (define (make-sum a1 a2)
-  (list '+
-        a1
-        (if (> (length a2) 2)
-            (make-sum (car a2) (cdr a2))
-            (car a2))))
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 
 
-(define (make-product m1 m2) (list '* m1 m2))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 (define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
 
 
 (define (make-exponentiation base exp)
@@ -33,10 +36,6 @@
 (define (exponent m) (caddr m))
 (define (exponentiation? x)
   (and (pair? x) (eq? (car x) '**)))
-
-
-
-;; main part 
 
 
 
@@ -64,4 +63,17 @@
          (error "unknow expression type -- DERIV" exp))))
 
 
-(deriv '(+ x 3 6) 'x)
+
+;; main part
+(define (augend s) 
+  (if (> (length s) 3) 
+      (cons '+ (cddr s)) 
+      (caddr s))) 
+
+(define (multiplicand s)
+  (if (> (length s) 3)
+      (cons '* (cddr s))
+      (caddr s)))
+
+(deriv '(+ x y (+ x 3)) 'x)
+(deriv '(* x y (+ x 3)) 'x)

73e074eea3c433f56ea4dbdb2fc3ad2005231692
cheng33c <asglitc12@gmail.com>
2017-08-11 16:40:30 +0800

    add : 2.58a.scm

--- chapter2/2.58a.scm
+++ chapter2/2.58a.scm
@@ -7,37 +7,17 @@
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
 
-(define (addend s) (cadr s))
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-(define (sum? x)
-  (and (pair? x) (eq? (car x) '+)))
+(define (augend s)
+  (if (> (length s) 3) 
+      (cons '+ (cddr s))
+      (caddr s)))
 
+(define (multiplicand s)
+  (if (> (length s) 3)
+      (cons '* (cddr s))
+      (caddr s)))
 
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-(define (product? x)
-  (and (pair? x) (eq? (car x) '*)))
-(define (multiplier p) (cadr p))
-
-
-(define (make-exponentiation base exp)
-  (cond ((= exp 0) 1)
-        ((= exp 1) base)
-        (else (list '** base exp))))
-(define (base m) (cadr m))
 (define (exponent m) (caddr m))
-(define (exponentiation? x)
-  (and (pair? x) (eq? (car x) '**)))
-
-
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -65,15 +45,39 @@
 
 
 ;; main part
-(define (augend s)
-  (if (> (length s) 3) 
-      (cons '+ (cddr s)) 
-      (caddr s))) 
 
-(define (multiplicand s)
-  (if (> (length s) 3)
-      (cons '* (cddr s))
-      (caddr s)))
+(define (addend s) (car s))
 
-(deriv '(+ x y (+ x 3)) 'x)
-(deriv '(* x y (+ x 3)) 'x)
+(define (multiplier p) (car p))
+
+(define (base m) (car m))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (cadr x) '**)))
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (make-exponentiation base exp)
+  (cond ((= exp 0) 1)
+        ((= exp 1) base)
+        (else (list base '** exp))))
+
+;; test part
+(deriv '(x + (3 * (x + (y + 2)))) 'x)

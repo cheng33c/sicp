0f7c0654618012cd0281081da09cd7c3d492b057
cheng33c <asglitc12@gmail.com>
2017-08-12 14:54:50 +0800

    add:element-of-set?.scm

---
+++ chapter2/element-of-set?.scm
@@ -0,0 +1,8 @@
+#lang planet neil/sicp
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(element-of-set? 10 '(1 2 10))

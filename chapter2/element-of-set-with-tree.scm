12312cd859963d10f85105a770d367285f8360c6
cheng33c <asglitc12@gmail.com>
2017-08-12 18:13:36 +0800

    element-of-set-with-tree.scm

---
+++ chapter2/element-of-set-with-tree.scm
@@ -0,0 +1,22 @@
+#lang planet neil/sicp
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+
+;; test
+(element-of-set? 5 (make-tree 1 (make-tree 3 nil nil) (make-tree 4 nil nil)))

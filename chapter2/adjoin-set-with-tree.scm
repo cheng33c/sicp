99aaeca381c14c877ec6dab54edfc442221a8438
cheng33c <asglitc12@gmail.com>
2017-08-12 18:13:03 +0800

    adjoin-with-tree.scm

---
+++ chapter2/adjoin-set-with-tree.scm
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+

397d817f46e5f11aaffc27ae5443fd5e2f04a56d
cheng33c <asglitc12@gmail.com>
2017-08-17 16:06:57 +0800

    2.64.scm

--- chapter2/2.66.scm
+++ chapter2/2.66.scm
@@ -7,11 +7,29 @@
 
 (define (right-branch tree) (caddr tree))
 
-(define (key my) (cdr my))
+(define (key my) (car my))
 
 ;; main
-(define (lookup given-key set-of-records)
-  (cond ((null? set-of-records) false)
-        ((equal? given-key (key (car set-of-records)))
-         (car set-of-records))
-        (else (lookup given-key (car set-of-records)))))
+(define (lookup given-key tree-of-records)
+  (if (null? tree-of-records)
+      #f
+      (let ((entry-key (key (entry tree-of-records))))
+        (cond ((equal? given-key entry-key) 
+               (entry tree-of-records))
+              ((> given-key entry-key)
+               (lookup given-key (right-branch tree-of-records)))
+              ((< given-key entry-key)
+               (lookup given-key (left-branch tree-of-records)))))))
+
+
+;; test
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define mytree (make-tree (list 30 'a)
+                          (make-tree (list 21 'b) nil nil)
+                          (make-tree (list 33 'c) nil nil)))
+
+
+(lookup 30 mytree)

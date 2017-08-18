0b689989b2047421ce3f15d09fddcbb286afd89c
cheng33c <asglitc12@gmail.com>
2017-08-18 13:16:39 +0800

    add:make-leaf-set.scm

---
+++ chapter2/make-leaf-set.scm
@@ -0,0 +1,47 @@
+#lang planet neil/sicp
+
+;; require
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (weight-leaf x) (caddr x))
+(define (symbol-leaf x) (cadr x))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (caddr tree)))
+
+;; main
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+;; test
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define my-pair (list (list 'a 3) (list 'b 6)))
+
+(make-leaf-set my-pair)

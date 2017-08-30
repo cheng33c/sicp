a061a15dda7df456bea50bb8d94aa447dec09386
cheng33c <asglitc12@gmail.com>
2017-08-31 00:17:50 +0800

    2.86.scm

---
+++ chapter2/2.86.scm
@@ -0,0 +1,64 @@
+#lang planet neil/sicp
+
+(define (sine x) (apply-generic 'sine x)) 
+(define (cosine x) (apply-generic 'cosine x)) 
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+
+  (put 'sine 'scheme-number
+       (lambda (x) (tag (sin x))))
+  (put 'cosine 'scheme-number
+       (lambda (x) (tag (cos x))))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+
+(define (install-rational-package)
+  ;; internal procedures
+  (define (number x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (number x) (denom y))
+                 (* (number y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (number x) (denom y))
+                 (* (number y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (number x) (denom y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (number x) (denom y))
+              (* (denom x) (number y))))
+
+  ;; interfact to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'sine 'rational
+       (lambda (x) (tag (sin x))))
+  (put 'cosine 'rational
+       (lambda (x) (tag (cos x))))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(ration rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)

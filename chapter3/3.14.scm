be04a3abd1ad6639c537a9727854ecb2e7db9580
cheng33c <asglitc12@gmail.com>
2017-09-05 22:07:58 +0800

    3.14.scm

---
+++ chapter3/3.14.scm
@@ -0,0 +1,12 @@
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+;; 输出x的回文
+;; v (a b c d)
+;; w (d c b a)

f60b03b1c70ef0e2694777f7507cce52235cb7b4
cheng33c <asglitc12@gmail.com>
2017-09-06 17:28:18 +0800

    3.17.scm

---
+++ chapter3/3.17.scm
@@ -0,0 +1,16 @@
+(define (count-pairs x)
+  (define (in-ls? ls x)
+    (cond ((null? ls) #f)
+          ((eq? (car ls) x) #t)
+          (else (in-ls (cdr ls) x))))
+  (let ((ls '()))
+    (define (iter current-x counter)
+      (cond ((null? current-x) counter)
+            ((in-ls? ls (car x)) counter)
+            (else
+             (iter (cdr current-x) (+ counter 1)))))
+    (iter x 0)))
+
+(define two (cons (cons 1 '()) '())) ;; 2
+(define three (cons (cons 1 '()) (cons 2 '()))) ;; 3
+(define seven (cons three three))

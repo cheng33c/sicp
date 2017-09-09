6ecc3776ded129d625c0f92d184ed7310a8fb165
cheng33c <asglitc12@gmail.com>
2017-09-09 14:53:04 +0800

    insert.scm

---
+++ chapter3/insert!.scm
@@ -0,0 +1,7 @@
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(set-cdr! record value)
+	(set-cdr! table
+		  (cons (cons key value) (cdr table)))))
+  'ok)

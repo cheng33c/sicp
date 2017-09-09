dc54b36833f5cdd2c84beaf6c8ba3db622bc2684
cheng33c <asglitc12@gmail.com>
2017-09-09 14:50:01 +0800

    lookup.scm

--- chapter3/3.23-2.scm
+++ chapter3/3.23-2.scm
@@ -16,13 +16,27 @@
   (if (pair? (cdr deque))
       (cadr deque)
       (error "ERROR reverse -- deque not have a reverse list" deque)))
+
+;;; when two-way-deque is empty, insert new-pair to front-list and reverse-list
+;;; to both forward-deque and reverse-deque
+;;; when two-way-deque is not empty, insert new-pair to front in forward-deque
+;;; insert new-pair to rear in reverse-deque
 (define (front-insert-deque! two-way-deque)
   (let ((new-pair (cons item '())))
     (cond ((empty-deque? deque)
-           (set-front-ptr! (forward two-way-deque) new-pair)
-           (set-rear-ptr! (forward two-way-deque) new-pair)
-           (set-front-ptr! (reverse two-way-deque) new-pair)
-           (set-rear-ptr! (reverse two-way-deque) new-pair))
+           (set-front-ptr! (forward-deque two-way-deque) new-pair)
+	   (set-rear-ptr! (forward-deque two-way-deque) new-pair)
+           (set-front-ptr! (reverse-deque two-way-deque) new-pair)
+	   (set-rear-ptr! (reverse-deque two-way-deque) new-pair))
           (else
-           (set-cdr! (rear-ptr (forward deque)) new-pair)
-           (set-cdr! ())))))
+           (set-front-ptr! (forward-deque two-way-deque) new-pair)
+           (set-rear-ptr! (reverse-deque two-way-deque) new-pair)))))
+
+(define (rear-insert-deque! two-way-deque)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-deque? deque)
+	   (set-rear-ptr! (forward-deque two-way-deque) new-pair)
+	   (set-front-ptr! (reverse-deque two-way-deque) new-pair))
+	  (else
+	   (set-rear-ptr! (forward-ptr two-way-deque) new-pair)
+	   (set-forward-ptr! (rear-ptr two-way-deque) new-pair)))))

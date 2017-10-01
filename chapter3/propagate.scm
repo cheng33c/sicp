43ce2ce2715c26cb88ff373c1162c4f00d94a1e7
cheng33c <asglitc12@gmail.com>
2017-10-01 16:35:55 +0800

    add:probe.scm

---
+++ chapter3/propagate.scm
@@ -0,0 +1,7 @@
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
